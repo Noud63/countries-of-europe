e03899ca025d3c64a5b2352b8304f979574dd7c4
Noud63 <noudvandun@gmail.com>
2021-07-14 14:06:42 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,126 @@
+// https://flagpedia.net/index
+// https://unsplash.com/s/photos/europe?orientation=landscape
+
+import { countries } from './countries.js'
+
+const images = ["city1.jpg", "city2.jpg", "city3.jpg", "city4.jpg", "city5.jpg", "city6.jpg", "city7.jpg", "city8.jpg", "city9.jpg", "city10.jpg"]
+const country = ["Czech republic", "Germany", "The Netherlands", "Italy", "Belgium", "France", "Spain", "United Kingdom", "Switzerland", "Iceland"]
+const flags = ["czech.png", "germany.png", "netherlands.png", "italy.png", "belgium.png", "france.png", "spain.png", "uk.png", "swiss.png", "iceland.png"]
+
+let index = 0
+let count = 1
+
+
+//Get weather
+const key = '31ba0dfb2a415daf8797fbb435b2f213'
+let getData = async (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${key}`;
+
+    try {
+        let data = await axios(url)
+        data = data
+        console.log(data)
+        let weather = {
+            temp: data.data.list[0].main.temp.toFixed(),
+            city: data.data.city.name,
+            des: data.data.list[0].weather[0].description,
+            icon: data.data.list[0].weather[0].icon,
+            wind: data.data.list[0].wind.speed.toFixed()
+        }
+
+        document.querySelector('.weatherData').innerHTML =
+            `<div>
+                <div>Temperature: ${Math.round(weather.temp) + ' \xB0' + "C"}</div>
+                <div>${weather.des}</div>
+            </div>
+             <div>
+                <img src="http://openweathermap.org/img/w/${weather.icon}.png" class="icon">
+            </div>
+           `
+    } catch (error) {
+        console.log(error)
+        document.querySelector('.weatherData').textContent = `Ooops no data!`
+    }
+}
+
+
+//add title flag and city pic
+function addHeader() {
+    document.querySelector('.bgImg').innerHTML = `<img src=images/${images[index]} class="bgImage">`;
+    document.querySelector('.country').textContent = count + ". " + country[index];
+    document.querySelector('.flag').innerHTML = `<img src=flags/${flags[index]}  style="width: 30px; display:block;">`
+}
+addHeader()
+
+
+//add info
+function addInfo() {
+    document.querySelector('.information').innerHTML =
+        `<div>Capitol: ${countries[index].capitol}</div>
+         <div>Population: ${countries[index].population}</div>
+         <div>Language: ${countries[index].language}</div>
+         <div>Currency: ${countries[index].currency}</div>
+        `
+    document.querySelector('.map').innerHTML = `<img src="${countries[index].image}">`
+    document.querySelector('.weatherHeader').textContent = `Current weather for: ${countries[index].capitol}`
+    getData(`${countries[index].capitol}`)
+}
+addInfo()
+
+
+const buttons = [...document.querySelectorAll('.btn')]
+buttons.forEach(button => {
+
+    button.addEventListener('click', function (e) {
+        if (e.target.textContent === "next") {
+            index++
+            count++
+        }
+
+        if (e.target.textContent === "prev") {
+            index--
+            count--
+        }
+
+        if (index > images.length - 1) {
+            index = 0
+            count = 1
+        }
+
+        if (index < 0) {
+            index = images.length - 1
+            count = images.length
+        }
+
+        addInfo()
+        addHeader()
+    })
+})
+
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth <= 500) {
+        document.getElementById('prev').classList.remove("prevBtn");
+        document.getElementById('next').classList.remove("nextBtn");
+    } else {
+        document.getElementById('prev').classList.add("prevBtn");
+        document.getElementById('next').classList.add("nextBtn");
+    }
+});
+
+
+const element = document.querySelector('.map')
+const events = ["mouseover", "mouseout"]
+events.forEach(event => {
+    element.addEventListener(event, function () {
+        if (event === "mouseover") {
+            document.querySelector('.mapOverlay').style.display = "flex";
+            document.querySelector('.mapOverlay').innerHTML = `<img src="${countries[index].imagebig}" class="mapPopup" >`
+        };
+
+        if (event === "mouseout") {
+            document.querySelector('.mapOverlay').style.display = "none";
+        }
+    })
+})
+
---
+++ src/js/countries.js
@@ -0,0 +1,92 @@
+export const countries = [
+    {
+        capitol: "Prague",
+        population: "10.729.258",
+        language: "Czech",
+        currency: "Euro (&#8364;)",
+        image: "images/czechred.png",
+        imagebig: "images/czechredbig.png"
+    },
+    {
+        capitol: "Berlin",
+        population: "83.840.000",
+        language: "German",
+        currency: "Euro (&#8364;)",
+        image: "images/germany.png",
+        imagebig: "images/germanyredbig.png"
+    },
+    {
+        capitol: "Amsterdam",
+        population: "17.173.564",
+        language: "Dutch",
+        currency: "Euro (&#8364;)",
+        image: "images/netherlandsred.png",
+        imagebig: "images/netherlandsredbig.png"
+
+    },
+    {
+        capitol: "Rome",
+        population: "59.400.000",
+        language: "Italian",
+        currency: "Euro (&#8364;)",
+        image: "images/italyred.png",
+        imagebig: "images/italyredbig.png"
+
+    },
+
+    {
+        capitol: "Brussels",
+        population: "11.632.326",
+        language: "Dutch/French/German",
+        currency: "Euro (&#8364;)",
+        image: "images/belgiumred.png",
+        imagebig: "images/belgiumredbig.png"
+    },
+
+    {
+        capitol: "Paris",
+        population: "67.000.000",
+        language: "French",
+        currency: "Euro (&#8364;)",
+        image: "images/francered.png",
+        imagebig: "images/franceredbig.png"
+    },
+
+    {
+        capitol: "Madrid",
+        population: "46.773.365",
+        language: "Spanish",
+        currency: "Euro (&#8364;)",
+        image: "images/spainred.png",
+        imagebig: "images/spainredbig.png"
+
+    },
+
+    {
+        capitol: "London",
+        population: "68.251.655",
+        language: "English",
+        currency: "Pound sterling (&#xa3;)",
+        image: "images/ukred.png",
+        imagebig: "images/ukredbig.png"
+    },
+
+    {
+        capitol: "Bern",
+        population: "8.719.662",
+        language: "German/French/Italian",
+        currency: "Swiss franc (&#x20A3;)",
+        image: "images/swissred.png",
+        imagebig: "images/swissredbig.png"
+    },
+
+    {
+        capitol: "Reykjavik",
+        population: "368.792",
+        language: "Icelandic",
+        currency: "Icelandic króna (Kr)",
+        image: "images/icelandred.png",
+        imagebig: "images/icelandredbig.png"
+    },
+
+]
